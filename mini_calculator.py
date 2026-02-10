19440e81a2c9912e09b2d49d8af2a4db395e6fdc
SanviSharma2018 <sharma.sanvi@zohomail.com>
2026-02-09 20:10:21 -0600

    Create mini_calculator.py

---
+++ mini_calculator.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter the 1st number: "))
+num2 = int(input("Enter the 2nd number: "))
+op = input("Enter the operator (+, -, *, /): ")
+
+if op == "+":
+    print("Result:", num1 + num2)
+
+
+
+
+
+
+
