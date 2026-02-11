eb12c97c391204671aa4649c2d134f6752623ce8
Sanvi Sharma <sharma.sanvi@zohomail.com>
2026-02-10 19:51:51 -0600

    Update mini_calculator.py

--- mini_calculator.py
+++ mini_calculator.py
@@ -1,3 +1,7 @@
+# take 2 number int(input())
+# take Operation
+# print result
+#import numbers
 num1 = int(input("Enter the 1st number: "))
 num2 = int(input("Enter the 2nd number: "))
 op = input("Enter the operator (+, -, *, /): ")
@@ -5,9 +9,16 @@ op = input("Enter the operator (+, -, *, /): ")
 if op == "+":
     print("Result:", num1 + num2)
 
+elif op == "-":
+    print("Result:", num1 - num2)
 
+elif op == "*":
+    print("Result:", num1 * num2)
 
-
-
-
-
+elif op == "/":
+    if num2 == 0:
+        print("Error: Division by zero is not allowed")
+    else:
+        print("Result:", num1 / num2)
+else:
+    print("Invalid operator")
