b354fd318dd990ff6cb044beb7b4b0a97e0b37d4
Sanvi Sharma <sharma.sanvi@zohomail.com>
2026-02-19 19:58:33 -0600

    Create Gym_By_Age.py

---
+++ Gym_By_Age.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age: "))
+
+if age > 14:
+    print("You can go to gym!!")
+elif age > 5:
+    print("you can go to gym with a grown up")
+
+elif age < 5:
+    print("Sorry, you cannot go to gym googleu mogleu")
