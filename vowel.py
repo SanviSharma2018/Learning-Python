6b4f4f366e544c6237160df210c43812a9e51282
Sanvi Sharma <sharma.sanvi@zohomail.com>
2026-03-04 19:47:38 -0600

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,6 @@
+letter = input("enter a letter ")
+
+if letter == "a" or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
+    print("it's a vowel!!")
+else:
+    print("it's not a vowel!!")
