03a5ab310eb05fd0880077cf36928f496200f218
Sanvi Sharma <sharma.sanvi@zohomail.com>
2026-03-30 19:11:48 -0500

    Create Mad_Libs_Story.py

---
+++ Mad_Libs_Story.py
@@ -0,0 +1,15 @@
+name   = input( "enter a name: ")
+food   = (input("enter a food: "))
+place  = (input("enter a place: " ))
+animal = (input("enter a animal: "))
+adjective = (input("enter a adjective: "))
+
+print(f"""
+      the story
+----------------------
+ {name} was {adjective}
+suddenly a {animal} came then
+they went to a restaurant
+then {name}💝 and the 💝{animal}
+    were best friends!!
+       the end!!!!!🩷💝""")
