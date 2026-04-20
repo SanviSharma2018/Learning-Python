eb2294d42d6a52edcf833869ebd014bc413c10a2
Sanvi Sharma <sharma.sanvi@zohomail.com>
2026-04-19 19:43:24 -0500

    Create name_greeter.py

---
+++ name_greeter.py
@@ -0,0 +1,9 @@
+
+name = input("enter a name ")
+
+print(f"hi,{name}!!")
+print("pick from the list")
+felling_list = ["sad","angery","happy","great"]
+print(felling_list)
+print = input("how are you?")
+print ("bey! bey!!!")
